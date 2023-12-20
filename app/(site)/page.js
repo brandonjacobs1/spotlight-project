752294d2c5f1603231abc9342a8f85890d1c7d92
Brandon Jacobs <jacobs27@student.byu.edu>
2023-12-19 23:47:16 -0700

    test ALB and pipeline

--- app/(site)/page.js
+++ app/(site)/page.js
@@ -7,7 +7,7 @@ export default function Home() {
     return (
         <Box component='main'>
             <CssBaseline />
-            <Typography variant="h2">16th Ward Spotlights</Typography>
+            <Typography variant="h2">16th Ward Spotlights v2</Typography>
         </Box>
     );
 }
