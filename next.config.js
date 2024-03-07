4e4caa8b3988ede42058c4c77e8375875d399c4c
Brandon Jacobs <jacobs27@student.byu.edu>
2024-03-07 10:24:39 -0700

    fix: next config

--- next.config.js
+++ next.config.js
@@ -1,40 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async rewrites() {
-        return [
-            {
-                source: '/api/:slug*',
-                destination: `https://localhost:3001/api/:slug*`,
-            },
-        ]
-    },
-    async headers() {
-        return [
-            {
-                // matching all API routes
-                source: "/api/:path*",
-                headers: [
-                    { key: "Access-Control-Allow-Credentials", value: "true" },
-                    { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
-                    { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
-                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-                ]
-            }
-        ]
-    },
     images: {
         remotePatterns: [
             {
                 protocol: 'https',
                 hostname: 'spotlight-16th-ward.s3.us-east-2.amazonaws.com',
-                port: '443',
+                port: '',
                 pathname: '/images/**',
             },
-            {
-                protocol: 'https',
-                hostname: 'localhost',
-                port: '3001'
-            }
         ],
     },
 }
